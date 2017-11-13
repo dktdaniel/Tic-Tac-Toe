6b510cac0854c6bd5612418fca24fee7a3eebef6
Daniel <dktdaniel@gmail.com>
2017-11-13 15:17:59 -0800

    Board

--- tictactoe/src/App.js
+++ tictactoe/src/App.js
@@ -2,7 +2,19 @@ import React, { Component } from 'react';
 import { Header, Table } from 'semantic-ui-react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      board: [[0,0,0], 
+              [0,0,0], 
+              [0,0,0]]
+    };
+  }
 
+  handleClick() {
+    
+  }
+  
   render() {
     return (
       <div className="App">
